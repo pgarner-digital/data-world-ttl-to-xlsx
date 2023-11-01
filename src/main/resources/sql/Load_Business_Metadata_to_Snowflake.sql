1ee88e5524f4fd559b0ec05f7a6d2997bd4c3b03
pgarner-digital <patrick.garner@digital.fl.gov>
2023-11-01 13:08:31 -0400

    Improved SQL queries

--- src/main/resources/sql/Load_Business_Metadata_to_Snowflake.sql
+++ src/main/resources/sql/Load_Business_Metadata_to_Snowflake.sql
@@ -133,13 +133,13 @@ select "orgId", count(*) from VIEW_METADATA group by "orgId" order by "orgId";
 select "orgId", count(*) from VIEW_COLUMN_METADATA group by "orgId" order by "orgId";
 select orgid, count(*) from LINKS_METADATA group by orgid order by OrgId;
 
-select "orgId", count(*) from DATABASE_METADATA_BAK group by "orgId" order by "orgId";
-select "orgId", count(*) from SCHEMA_METADATA_BAK group by "orgId" order by "orgId";
-select "orgId", count(*) from TABLE_METADATA_BAK group by "orgId" order by "orgId";
-select "orgId", count(*) from COLUMN_METADATA_BAK group by "orgId" order by "orgId";
-select "orgId", count(*) from VIEW_METADATA_BAK group by "orgId" order by "orgId";
-select "orgId", count(*) from VIEW_COLUMN_METADATA_BAK group by "orgId" order by "orgId";
-select orgid, count(*) from LINKS_METADATA_BAK group by orgid order by OrgId;
+select "orgId", count(*) from DATABASE_METADATA_PROD group by "orgId" order by "orgId";
+select "orgId", count(*) from SCHEMA_METADATA_PROD group by "orgId" order by "orgId";
+select "orgId", count(*) from TABLE_METADATA_PROD group by "orgId" order by "orgId";
+select "orgId", count(*) from COLUMN_METADATA_PROD group by "orgId" order by "orgId";
+select "orgId", count(*) from VIEW_METADATA_PROD group by "orgId" order by "orgId";
+select "orgId", count(*) from VIEW_COLUMN_METADATA_PROD group by "orgId" order by "orgId";
+select orgid, count(*) from LINKS_METADATA_PROD group by orgid order by OrgId;
 
 
 -- delete from DATABASE_METADATA where "orgId" = 'DHSMV';
@@ -151,123 +151,94 @@ select orgid, count(*) from LINKS_METADATA_BAK group by orgid order by OrgId;
 -- delete from LINKS_METADATA where OrgId = 'DHSMV';
 
 select "com.infa.odin.models.relational.PrimaryKeyColumn", count(*)
-from COLUMN_METADATA_BAK
+from COLUMN_METADATA
 group by "com.infa.odin.models.relational.PrimaryKeyColumn";
 
 select count(*)
-from COLUMN_METADATA_BAK
+from COLUMN_METADATA
 where "com.infa.odin.models.relational.PrimaryKeyColumn" = "core.name";
 -- 2185
 
 select count(*)
-from COLUMN_METADATA_BAK
+from COLUMN_METADATA
 where "com.infa.odin.models.relational.PrimaryKeyColumn" = 'Yes';
 
 
-update COLUMN_METADATA_BAK
+update COLUMN_METADATA_PROD
     set "com.infa.odin.models.relational.PrimaryKeyColumn" = 'Yes'
 where
     "com.infa.odin.models.relational.PrimaryKeyColumn" = "core.name";
-update COLUMN_METADATA_BAK
+update COLUMN_METADATA_PROD
     set "com.infa.odin.models.relational.PrimaryKeyColumn" = null
 where
     "com.infa.odin.models.relational.PrimaryKeyColumn" <> 'Yes';
 
 
-select "orgId", count(*) from SCHEMA_METADATA_BAK where "core.name" like '%PF3%' group by "orgId" order by "orgId";
-update SCHEMA_METADATA_BAK set "core.name" = '(unavailable)' where "core.name" like '%PF3%';
-select "orgId", count(*) from SCHEMA_METADATA_BAK where "core.name" = '(unavailable)' group by "orgId" order by "orgId";
-select * from SCHEMA_METADATA_BAK where "core.name" = '(unavailable)';
+select "orgId", count(*) from SCHEMA_METADATA_PROD where "core.name" like '%PF3%' group by "orgId" order by "orgId";
+--update SCHEMA_METADATA_PROD set "core.name" = '(unavailable)' where "core.name" like '%PF3%';
+select "orgId", count(*) from SCHEMA_METADATA_PROD where "core.name" = '(unavailable)' group by "orgId" order by "orgId";
+select * from SCHEMA_METADATA_PROD where "core.name" = '(unavailable)';
 
 
-select * from COLUMN_METADATA_BAK where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
+select * from COLUMN_METADATA_PROD where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
 select * from COLUMN_METADATA where "com.infa.odin.models.relational.PrimaryKeyColumn" = 'Yes';
-select count(*) from COLUMN_METADATA_BAK where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
-select count(*) from COLUMN_METADATA_BAK where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
+select count(*) from COLUMN_METADATA_PROD where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
+select count(*) from COLUMN_METADATA_PROD where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
 
-
-select * from TABLE_METADATA where "core.name" = 'TMP_20200918_LOAD_DOB';
-select * from COLUMN_METADATA where "core.description" like '%,%';
-select * from LINKS_METADATA where Source = 'sch.72624a0ca14946ee918a4f916f435b09';
-select distinct OrgId from LINKS_METADATA;
-select * from COLUMN_METADATA where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
-select * from COLUMN_METADATA where "com.infa.odin.models.relational.PrimaryKeyColumn" = "core.name" order by "core.name" ;
-select COUNT(DISTINCT SOURCE) from LINKS_METADATA_BAK WHERE orgId = 'DMS' and ASSOCIATION = 'com.infa.odin.models.relational.ViewToViewColumn';
-update COLUMN_METADATA set "com.infa.odin.models.relational.PrimaryKeyColumn" = 'Yes' where "com.infa.odin.models.relational.PrimaryKeyColumn" = "core.name";
-update COLUMN_METADATA set "com.infa.odin.models.relational.PrimaryKeyColumn" = null where "com.infa.odin.models.relational.PrimaryKeyColumn" <> 'Yes';
-select * from COLUMN_METADATA where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
-select count(*) from COLUMN_METADATA;
-select count(*) from COLUMN_METADATA_BAK;
-
--- 4118
 
 -- SELECT COUNT(DISTINCT t1.SOURCE)
--- FROM LINKS_METADATA_BAK t1
---          INNER JOIN VIEW_METADATA_BAK t2
+-- FROM LINKS_METADATA_PROD t1
+--          INNER JOIN VIEW_METADATA_PROD t2
 --                     ON t1.SOURCE = t2."core.externalId";
 -- 4118
 
 -- SELECT COUNT(DISTINCT t1.TARGET)
--- FROM LINKS_METADATA_BAK t1
---          INNER JOIN VIEW_COLUMN_METADATA_BAK t2
+-- FROM LINKS_METADATA_PROD t1
+--          INNER JOIN VIEW_COLUMN_METADATA_PROD t2
 --                     ON t1.TARGET = t2."core.externalId";
 --65012
 
-select * from TABLE_METADATA WHERE "core.description" like '%,%';
+-- create or replace table DATABASE_METADATA_PROD as SELECT * FROM DATABASE_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table SCHEMA_METADATA_PROD as SELECT * FROM SCHEMA_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table TABLE_METADATA_PROD as SELECT * FROM TABLE_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table COLUMN_METADATA_PROD as SELECT * FROM COLUMN_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table VIEW_METADATA_PROD as SELECT * FROM VIEW_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table VIEW_COLUMN_METADATA_PROD as SELECT * FROM VIEW_COLUMN_METADATA_BAK where "orgId" <> 'DOE';
+-- create or replace table LINKS_METADATA_PROD as SELECT * FROM LINKS_METADATA_BAK where orgId <> 'DOE';
+
+select * from DATABASE_METADATA_PROD;
+select * from SCHEMA_METADATA_PROD;
+select * from TABLE_METADATA_PROD;
+select * from COLUMN_METADATA_PROD;
+select * from VIEW_METADATA_PROD;
+select * from VIEW_COLUMN_METADATA_PROD;
+select * from LINKS_METADATA_PROD;
+
+select count(*) from DATABASE_METADATA_PROD;
+select count(*) from SCHEMA_METADATA_PROD;
+select count(*) from TABLE_METADATA_PROD;
+select count(*) from COLUMN_METADATA_PROD;
+select count(*) from LINKS_METADATA_PROD;
+select count(*) from VIEW_METADATA_PROD;
+select count(*) from VIEW_COLUMN_METADATA_PROD;
 
 
-
-select distinct "custom.data.world.import.sensitiveData" from VIEW_METADATA_BAK;
-select distinct "custom.data.world.import.sensitiveData" from VIEW_COLUMN_METADATA_BAK;
-select distinct "custom.data.world.import.restrictedToPublic" from VIEW_METADATA_BAK;
-select distinct "custom.data.world.import.restrictedToPublic" from VIEW_COLUMN_METADATA_BAK;
-
-desc table VIEW_COLUMN_METADATA;
-desc table VIEW_METADATA;
-
-
-create or replace table DATABASE_METADATA_BAK as SELECT * FROM DATABASE_METADATA where "orgId" <> 'DOE';
-create or replace table SCHEMA_METADATA_BAK as SELECT * FROM SCHEMA_METADATA where "orgId" <> 'DOE';
-create or replace table TABLE_METADATA_BAK as SELECT * FROM TABLE_METADATA where "orgId" <> 'DOE';
-create or replace table COLUMN_METADATA_BAK as SELECT * FROM COLUMN_METADATA where "orgId" <> 'DOE';
-create or replace table VIEW_METADATA_BAK as SELECT * FROM VIEW_METADATA where "orgId" <> 'DOE';
-create or replace table VIEW_COLUMN_METADATA_BAK as SELECT * FROM VIEW_COLUMN_METADATA where "orgId" <> 'DOE';
-create or replace table LINKS_METADATA_BAK as SELECT * FROM LINKS_METADATA where orgId <> 'DOE';
-
-select * from DATABASE_METADATA_BAK;
-select * from SCHEMA_METADATA_BAK;
-select * from TABLE_METADATA_BAK;
-select * from COLUMN_METADATA_BAK where "core.externalId" like 'col.aef0243ec45%';
-select * from COLUMN_METADATA_BAK where "com.infa.odin.models.relational.PrimaryKeyColumn" is not null;
-select * from LINKS_METADATA_BAK;
-select * from VIEW_METADATA_BAK;
-select * from VIEW_COLUMN_METADATA_BAK;
-
-select count(*) from DATABASE_METADATA_BAK;
-select count(*) from SCHEMA_METADATA_BAK;
-select count(*) from TABLE_METADATA_BAK;
-select count(*) from COLUMN_METADATA_BAK;
-select count(*) from LINKS_METADATA_BAK;
-select count(*) from VIEW_METADATA_BAK;
-select count(*) from VIEW_COLUMN_METADATA_BAK;
-
-
-select * from DATABASE_METADATA_BAK;
-select * from SCHEMA_METADATA_BAK;
-select * from TABLE_METADATA_BAK;
-select * from COLUMN_METADATA_BAK;
-select * from VIEW_METADATA_BAK;
-select * from VIEW_COLUMN_METADATA_BAK;
-select * from LINKS_METADATA_BAK;
+select * from DATABASE_METADATA_PROD;
+select * from SCHEMA_METADATA_PROD;
+select * from TABLE_METADATA_PROD;
+select * from COLUMN_METADATA_PROD;
+select * from VIEW_METADATA_PROD;
+select * from VIEW_COLUMN_METADATA_PROD;
+select * from LINKS_METADATA_PROD;
 
 -- SELECT t1.*
--- FROM TABLE_METADATA_BAK t1
---          INNER JOIN VIEW_METADATA_BAK t2
+-- FROM TABLE_METADATA_PROD t1
+--          INNER JOIN VIEW_METADATA_PROD t2
 --                     ON t1."core.externalId" = t2."core.externalId";
 --
 -- SELECT t1.*
--- FROM COLUMN_METADATA_BAK t1
---          INNER JOIN VIEW_COLUMN_METADATA_BAK t2
+-- FROM COLUMN_METADATA_PROD t1
+--          INNER JOIN VIEW_COLUMN_METADATA_PROD t2
 --                     ON t1."core.externalId" = t2."core.externalId";
 
 select distinct "com.infa.odin.models.relational.Datatype" from VIEW_COLUMN_METADATA;
